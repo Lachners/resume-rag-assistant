cc65b10b72753a38f3bdf767486f60cb4d7c5eb7
Lachners <92989286+Lachners@users.noreply.github.com>
2025-04-11 14:30:27 +0200

    Add files via upload

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+DB_CONFIG = {
+    "dbname": "jobs",
+    "user":"seblach",
+    "password":"localhost123",
+    "host":"localhost",
+    "port": 5432,
+}
+
+OLLAMA_MODEL = "deepseek-coder:1.5"
+EMBEDDING_MODEL = "all-MiniLM-L6-v2"
