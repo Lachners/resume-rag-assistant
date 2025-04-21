63848f8a49d8ae5456914b95784c5f8db42f6ae1
Lachners <92989286+Lachners@users.noreply.github.com>
2025-04-21 15:03:34 +0200

    Add files via upload

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+DB_CONFIG = {
+    "dbname": "jobs",
+    "user":"user",
+    "password":"password",
+    "host":"localhost",
+    "port": 5432,
+}
+
+OLLAMA_MODEL = "deepseek-r1:7b"
+EMBEDDING_MODEL = "all-mpnet-base-v2"
